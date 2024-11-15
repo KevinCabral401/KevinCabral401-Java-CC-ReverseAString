7a4f04240517f3cfcd63b95e97352cbe852b03ee
Kevin Cabral <kevincabral401@gmail.com>
2024-11-15 23:50:55 +0000

    Completed Coding Challange StringReverse

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -13,6 +13,7 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+        String reversed = new StringBuilder(original).reverse().toString();
+        return reversed;
     }
 }
